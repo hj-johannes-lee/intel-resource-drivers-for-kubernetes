957c6af442d116d1e5add9b2155ed14eeded8d6e
Tuomas Katila <tuomas.katila@intel.com>
2024-08-15 18:54:40 +0300

    gpu: add by-path mounts
    Signed-off-by: Tuomas Katila <tuomas.katila@intel.com>

--- pkg/gpu/cdihelpers/cdihelpers.go
+++ pkg/gpu/cdihelpers/cdihelpers.go
@@ -18,7 +18,9 @@ package cdihelpers
 
 import (
 	"fmt"
+	"os"
 	"path"
+	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
@@ -219,7 +221,29 @@ func AddDevicesToSpec(devices device.DevicesInfo, spec *specs.Spec) {
 				},
 			)
 		}
-		// TODO: add /dev/dri/by-path entries
+
+		addBypathMounts(device, &newDevice, dridevpath)
+
 		spec.Devices = append(spec.Devices, newDevice)
+	}
+}
+
+// Add GPU specific by-path mounts to the spec.
+func addBypathMounts(info *device.DeviceInfo, spec *specs.Device, dridevPath string) {
+	bypathPath := filepath.Join(dridevPath, "by-path")
+
+	basename := filepath.Join(bypathPath, fmt.Sprintf("pci-%s-", info.PCIAddress))
+
+	gpuFiles := []string{basename + "card", basename + "render"}
+
+	for _, gpuFile := range gpuFiles {
+		if _, err := os.Stat(gpuFile); err == nil {
+			spec.ContainerEdits.Mounts = append(spec.ContainerEdits.Mounts, &specs.Mount{
+				HostPath:      gpuFile,
+				ContainerPath: gpuFile,
+				Type:          "none",
+				Options:       []string{"bind", "rw"},
+			})
+		}
 	}
 }
