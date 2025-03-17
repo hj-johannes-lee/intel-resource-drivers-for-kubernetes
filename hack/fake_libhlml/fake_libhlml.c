7cd639bb70f23dc711dc0ce5634e50def24c8252
Alexey Fomenko <alexey.fomenko@intel.com>
2025-03-17 09:59:33 +0200

    Replace printfs with macro
    Co-authored-by: Eero Tamminen <eero.t.tamminen@intel.com>

--- hack/fake_libhlml/fake_libhlml.c
+++ hack/fake_libhlml/fake_libhlml.c
@@ -38,6 +38,8 @@ struct main_struct_t {
     struct device_info_t devices_info[MAX_DEVICES];
 } main_struct;
 
+static void log_call(const char *name) { printf("%s called\n", name); }
+
 // custom_init is called from a test function to populate the main_struct
 // with fake information that otherwise would have been deduced from the
 // sysfs by a real HLML library.
@@ -69,26 +71,26 @@ void reset() {
 
 /* supported APIs */
 hlml_return_t hlml_init(void) {
-    printf("hlml_init called\n");
+    log_call(__func__);
     return hlml_init_with_flags(0);
 };
 
 hlml_return_t hlml_init_with_flags(unsigned int flags) {
-    printf("hlml_init_with_flags called\n");
+    log_call(__func__);
     main_struct.initialized = true;
 
     return HLML_SUCCESS;
 };
 
 hlml_return_t hlml_shutdown(void) {
-    printf("hlml_shutdown called\n");
+    log_call(__func__);
     main_struct.initialized = false;
 
     return HLML_SUCCESS;
 };
 
 hlml_return_t hlml_device_get_count(unsigned int *device_count) {
-    printf("hlml_device_get_count called\n");
+    log_call(__func__);
     if (!device_count)
         return HLML_ERROR_INVALID_ARGUMENT;
     *device_count = main_struct.devices_num;
@@ -96,7 +98,7 @@ hlml_return_t hlml_device_get_count(unsigned int *device_count) {
 };
 
 hlml_return_t hlml_device_get_handle_by_pci_bus_id(const char *pci_addr, hlml_device_t *device) {
-    printf("hlml_device_get_handle_by_pci_bus_id called\n");
+    log_call(__func__);
 
     struct device_info_t *device_info;
 
@@ -119,7 +121,7 @@ hlml_return_t hlml_device_get_handle_by_pci_bus_id(const char *pci_addr, hlml_de
 };
 
 hlml_return_t hlml_device_get_handle_by_index(unsigned int index, hlml_device_t *device) {
-    printf("hlml_device_get_handle_by_index called\n");
+    log_call(__func__);
 
     struct device_info_t *device_info;
 
@@ -140,18 +142,18 @@ hlml_return_t hlml_device_get_handle_by_index(unsigned int index, hlml_device_t 
 };
 
 hlml_return_t hlml_device_get_handle_by_UUID (const char* uuid, hlml_device_t *device) {
-    printf("hlml_device_get_handle_by_UUID called\n");
+    log_call(__func__);
     return HLML_SUCCESS;
 };
 
 hlml_return_t hlml_device_get_name(hlml_device_t device, char *name,
                    unsigned int  length) {
-    printf("hlml_device_get_name called\n");
+    log_call(__func__);
     return HLML_SUCCESS;
 };
 
 hlml_return_t hlml_device_get_pci_info(hlml_device_t device, hlml_pci_info_t *pci) {
-    printf("hlml_device_get_pci_info called\n");
+    log_call(__func__);
 
     if (!main_struct.initialized)
         return HLML_ERROR_UNINITIALIZED;
@@ -172,101 +174,101 @@ hlml_return_t hlml_device_get_pci_info(hlml_device_t device, hlml_pci_info_t *pc
 hlml_return_t hlml_device_get_clock_info(hlml_device_t device,
                      hlml_clock_type_t type,
                      unsigned int *clock) {
-    printf("hlml_device_get_clock_info called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_max_clock_info(hlml_device_t device,
                          hlml_clock_type_t type,
                          unsigned int *clock) {
-    printf("hlml_device_get_max_clock_info called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_clock_limit_info(hlml_device_t device,
                                             hlml_clock_type_t type,
                                             unsigned int *clock) {
-    printf("hlml_device_get_clock_limit_info called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_utilization_rates(hlml_device_t device,
                     hlml_utilization_t *utilization) {
-    printf("hlml_device_get_utilization_rates called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_memory_info(hlml_device_t device, hlml_memory_t *memory) {
-    printf("hlml_device_get_memory_info called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_temperature(hlml_device_t device,
                       hlml_temperature_sensors_t sensor_type,
                       unsigned int *temp) {
-    printf("hlml_device_get_temperature called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_temperature_threshold(hlml_device_t device,
                 hlml_temperature_thresholds_t threshold_type,
                 unsigned int *temp) {
-    printf("hlml_device_get_temperature_threshold called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_persistence_mode(hlml_device_t device,
                         hlml_enable_state_t *mode) {
-    printf("hlml_device_get_persistence_mode called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_performance_state(hlml_device_t device,
                         hlml_p_states_t *p_state) {
-    printf("hlml_device_get_performance_state called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_supported_performance_states(hlml_device_t device,
                                                        hlml_p_states_t *pstates,
                                                        unsigned int size) {
-    printf("hlml_device_get_supported_performance_states called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_power_usage(hlml_device_t device,
                       unsigned int *power) {
-    printf("hlml_device_get_power_usage called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_power_management_mode(hlml_device_t device,
                                                    hlml_enable_state_t *state) {
-    printf("hlml_device_get_power_management_mode called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_power_management_limit(hlml_device_t device,
                                                     unsigned int *limit) {
-    printf("hlml_device_get_power_management_limit called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 }
 
 hlml_return_t hlml_device_set_power_management_limit(hlml_device_t device, unsigned int limit) {
-    printf("hlml_device_set_power_management_limit called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 }
 
 hlml_return_t hlml_device_get_power_management_default_limit(hlml_device_t device,
                         unsigned int *default_limit) {
-    printf("hlml_device_get_power_management_default_limit called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_ecc_mode(hlml_device_t device,
                        hlml_enable_state_t *current,
                        hlml_enable_state_t *pending) {
-    printf("hlml_device_get_ecc_mode called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
@@ -274,7 +276,7 @@ hlml_return_t hlml_device_get_total_ecc_errors(hlml_device_t device,
                     hlml_memory_error_type_t error_type,
                     hlml_ecc_counter_type_t counter_type,
                     unsigned long long *ecc_counts) {
-    printf("hlml_device_get_total_ecc_errors called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
@@ -283,20 +285,20 @@ hlml_return_t hlml_device_get_memory_error_counter(hlml_device_t device,
                     hlml_ecc_counter_type_t counter_type,
                     hlml_memory_location_type_t location,
                     unsigned long long *ecc_counts) {
-    printf("hlml_device_get_memory_error_counter called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_uuid(hlml_device_t device,
                    char *uuid,
                    unsigned int length) {
-    printf("hlml_device_get_uuid called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_minor_number(hlml_device_t device,
                        unsigned int *minor_number) {
-    printf("hlml_device_get_minor_number called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
@@ -304,26 +306,26 @@ hlml_return_t hlml_device_get_minor_number(hlml_device_t device,
 hlml_return_t hlml_device_register_events(hlml_device_t device,
                       unsigned long long event_types,
                       hlml_event_set_t set) {
-    printf("hlml_device_register_events called\n");
+    log_call(__func__);
     return HLML_ERROR_INVALID_ARGUMENT;
 };
 
 // TODO: implement registration and events sending
 hlml_return_t hlml_event_set_create(hlml_event_set_t *set) {
-    printf("hlml_event_set_create called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 // TODO: implement registration and events sending
 hlml_return_t hlml_event_set_free(hlml_event_set_t set) {
-    printf("hlml_event_set_free called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_event_set_wait(hlml_event_set_t set,
                   hlml_event_data_t *data,
                   unsigned int timeoutms) {
-    printf("hlml_event_set_wait called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
@@ -332,22 +334,22 @@ hlml_return_t hlml_device_get_mac_info(hlml_device_t device,
                        unsigned int mac_info_size,
                        unsigned int start_mac_id,
                        unsigned int *actual_mac_count) {
-    printf("hlml_device_get_mac_info called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_hl_revision(hlml_device_t device, int *hl_revision) {
-    printf("hlml_device_get_hl_revision called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_pcb_info(hlml_device_t device, hlml_pcb_info_t *pcb) {
-    printf("hlml_device_get_pcb_info called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_serial(hlml_device_t device, char *serial, unsigned int length) {
-    printf("hlml_device_get_serial called\n");
+    log_call(__func__);
 
     if (SERIAL_MAX > (int)length)
         return HLML_ERROR_INSUFFICIENT_SIZE;
@@ -359,75 +361,75 @@ hlml_return_t hlml_device_get_serial(hlml_device_t device, char *serial, unsigne
 };
 
 hlml_return_t hlml_device_get_module_id(hlml_device_t device, unsigned int *module_id) {
-    printf("hlml_device_get_module_id called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_board_id(hlml_device_t device, unsigned int* board_id) {
-    printf("hlml_device_get_board_id called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_pcie_throughput(hlml_device_t device,
                           hlml_pcie_util_counter_t counter,
                           unsigned int *value) {
-    printf("hlml_device_get_pcie_throughput called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_pcie_replay_counter(hlml_device_t device, unsigned int *value) {
-    printf("hlml_device_get_pcie_replay_counter called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_curr_pcie_link_generation(hlml_device_t device,
                             unsigned int *curr_link_gen) {
-    printf("hlml_device_get_curr_pcie_link_generation called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_curr_pcie_link_width(hlml_device_t device,
                            unsigned int *curr_link_width) {
-    printf("hlml_device_get_curr_pcie_link_width called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_current_clocks_throttle_reasons(hlml_device_t device,
         unsigned long long *clocks_throttle_reasons) {
-    printf("hlml_device_get_current_clocks_throttle_reasons called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_total_energy_consumption(hlml_device_t device,
         unsigned long long *energy) {
-    printf("hlml_device_get_total_energy_consumption called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_get_mac_addr_info(hlml_device_t device, uint64_t *mask, uint64_t *ext_mask) {
-    printf("hlml_get_mac_addr_info called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_nic_get_link(hlml_device_t device, uint32_t port, bool *up) {
-    printf("hlml_nic_get_link called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_nic_get_statistics(hlml_device_t device, hlml_nic_stats_info_t *stats_info) {
-    printf("hlml_nic_get_statistics called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_clear_cpu_affinity(hlml_device_t device) {
-    printf("hlml_device_clear_cpu_affinity called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_cpu_affinity(hlml_device_t device,
                        unsigned int cpu_set_size,
                        unsigned long *cpu_set) {
-    printf("hlml_device_get_cpu_affinity called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
@@ -435,7 +437,7 @@ hlml_return_t hlml_device_get_cpu_affinity_within_scope(hlml_device_t device,
                             unsigned int cpu_set_size,
                             unsigned long *cpu_set,
                             hlml_affinity_scope_t scope) {
-    printf("hlml_device_get_cpu_affinity_within_scope called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
@@ -443,19 +445,19 @@ hlml_return_t hlml_device_get_memory_affinity(hlml_device_t device,
                           unsigned int node_set_size,
                           unsigned long *node_set,
                           hlml_affinity_scope_t scope) {
-    printf("hlml_device_get_memory_affinity called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_set_cpu_affinity(hlml_device_t device) {
-    printf("hlml_device_set_cpu_affinity called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_violation_status(hlml_device_t device,
                            hlml_perf_policy_type_t perf_policy_type,
                            hlml_violation_time_t *viol_time) {
-    printf("hlml_device_get_violation_status called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
@@ -463,69 +465,69 @@ hlml_return_t hlml_device_get_replaced_rows(hlml_device_t device,
                         hlml_row_replacement_cause_t cause,
                         unsigned int *row_count,
                         hlml_row_address_t *addresses) {
-    printf("hlml_device_get_replaced_rows called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_replaced_rows_pending_status(hlml_device_t device,
                                hlml_enable_state_t *is_pending) {
-    printf("hlml_device_get_replaced_rows_pending_status called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_get_hlml_version(char *version, unsigned int length) {
-    printf("hlml_get_hlml_version called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_get_driver_version(char *driver_version, unsigned int length) {
-    printf("hlml_get_driver_version called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_get_nic_driver_version(char *driver_version, unsigned int length) {
-    printf("hlml_get_nic_driver_version called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 }
 
 hlml_return_t hlml_get_model_number(hlml_device_t device, char *model_number,
                     unsigned int length) {
-    printf("hlml_get_model_number called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_get_firmware_fit_version(hlml_device_t device, char *firmware_fit,
                         unsigned int length) {
-    printf("hlml_get_firmware_fit_version called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_get_firmware_spi_version(hlml_device_t device, char *firmware_spi,
                         unsigned int length) {
-    printf("hlml_get_firmware_spi_version called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_get_fw_boot_version(hlml_device_t device, char *fw_boot_version,
                        unsigned int length) {
-    printf("hlml_get_fw_boot_version called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_get_fw_os_version(hlml_device_t device, char *fw_os_version,
                      unsigned int length) {
-    printf("hlml_get_fw_os_version called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_get_cpld_version(hlml_device_t device, char *cpld_version,
                     unsigned int length) {
-    printf("hlml_get_cpld_version called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
 
 hlml_return_t hlml_device_get_oper_status(hlml_device_t device, char *status,
                                          unsigned int length) {
-    printf("hlml_device_get_oper_status called\n");
+    log_call(__func__);
     return HLML_ERROR_NOT_SUPPORTED;
 };
